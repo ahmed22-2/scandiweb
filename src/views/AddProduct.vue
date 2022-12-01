73b00bd528cf64467f920d63a1c0a064102bc3b7
hamdy2207 <hamdymohammed2207@gmail.com>
2022-12-01 16:42:55 +0200

    version-1

---
+++ src/views/AddProduct.vue
@@ -0,0 +1,209 @@
+<template>
+    <div class="container">
+        <header>
+            <div>
+                <h2>Product Add</h2>
+            </div>
+             <div class="btns-header">                
+                <button class="btn" @click="addProduct()" type="Submit">Save</button>
+
+                <button class="btn" @click="back()">Cancel</button>
+            </div>
+        </header>
+        <hr>
+        <div class="errors">
+            <span v-for="error in errors" :key="error.id">{{error}}<br></span>
+        </div>
+        <div class="form" >
+            <form action="" id="product_form" @submit.prevent="addProduct()">
+                <div>
+                    <div class="inp_form">
+                        <label for="sku">SKU</label>
+                        <input type="text" id="sku"  placeholder="#sku" v-model="sku" >
+                    </div>
+                    <div class="inp_form">
+                        <label for="name">Name</label>
+                        <input type="text" id="name" placeholder="#name" v-model="name" >
+                    </div>
+                    <div class="inp_form">
+                        <label for="price">Price ($)</label>
+                        <input type="text" id="price" placeholder="#price" v-model="price" >
+                    </div>
+                </div>
+
+
+                <div>
+                    <select v-model="type" name="type" id="productType" @change="getAttributes($event)" >
+                            <option disabled value="0" selected>type</option>
+                            <option :key="item.id" v-for="item in types" :value="item.id" :id="item.name" >{{item.name}}</option>
+                    </select>
+                </div>
+
+                <div  class="inp_form" v-for="attribute in attributes" :key="attribute.id" :id="attribute.id">
+                    <label :for="attribute.name" >{{attribute.name}} ({{attribute.unit}}) </label> 
+                    <input :id="attribute.name"  class="input-field" name="field1" type="text"   v-model="attribute.value">
+                </div>
+                <p>{{type.description}}</p>
+
+            </form>
+        </div>
+    </div>
+</template>
+
+
+
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: "AddProduct",
+  data(){
+      return {
+          name:'',
+          sku:'',
+          price: '',
+          type: '0',
+          errors: [],
+          attributes: [],
+          type_value: 'Type',
+          types: [],
+          err: [],
+          host: "https://scandi-task-api.000webhostapp.com/"
+          }
+          
+      },
+  methods: { 
+          back(){
+                  this.$router.push("/");
+              },
+          addProduct(){
+                  var data = new FormData();
+                  data.append("sku", this.sku);
+                  data.append("name", this.name);
+                  data.append("price", this.price);
+                  data.append("type", this.type_value);
+                  data.append("attributes", JSON.stringify(this.attributes));
+                  for (const key in this.attributes) {
+                          data.append(this.attributes[key].name,this.attributes[key].value);
+                      }
+                  axios
+                  .post(this.host+'back_end/public/home/addProduct',data)
+                  .then(res => { 
+                      if(res.data.errors){
+                          this.errors = res.data.errors;
+                      }else{
+                          this.back();
+                      }
+                  })
+                  .catch(err => (this.err = err.data))
+              
+
+
+          }
+          ,getAttributes(event){
+              let type_id = event.target.value;
+              this.type_value = event.target.options[event.target.options.selectedIndex].text;
+              axios
+              .get(this.host+'back_end/public/home/getAttributes&type_id='+type_id)
+              .then(res =>{
+                  (this.attributes = res.data)
+                  })
+              .catch(err => {console.log("Error", err);});
+                  
+          }
+        
+
+  },mounted()
+  {
+      // this.getTypes();          
+              axios
+              .get(this.host+'back_end/public/home/getTypes')
+              .then(res => {
+                  this.types = res.data;
+              })
+              .catch(err => {console.log("Error", err);});
+
+        
+      
+
+  }
+}
+
+</script>
+
+
+<style scoped>
+.container {
+    position: relative;
+    height: 100%;
+    width: 100%;
+    overflow: hidden;
+  }
+
+header{
+  margin-top: 50px;
+    height: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+.btns-header {
+  margin-right: 50px;
+    width: 20%;
+    display: flex;
+    justify-content: space-around;
+}
+header h2 {
+    margin-left: 20px; 
+}
+.btn {
+  color: #fff;
+  border: none;
+  background-color: #2298e6;
+  padding: 5px;
+  cursor: pointer;
+}
+.btn a {
+    text-decoration: none;
+    color: #fff;
+}
+
+form {
+  width: 200px;
+  margin: 50px 20px;
+}
+form div {
+    margin: 20px 0;
+}
+form input,select {
+    margin-bottom: 5px;
+    padding: 4px;
+    border: 1px solid #2298e6;
+    outline: #2298e6;
+}
+::placeholder{
+  color: rgb(88, 81, 81);
+}
+form input {
+  margin-left: 20px;
+  position: absolute;
+  left: 20%;
+}
+
+.errors {
+    position:absolute;
+    margin: 25px 500px;
+    color: red;
+} 
+.inp_form {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+}
+
+
+
+
+</style>
---
+++ src/views/ProductList.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="container">
+      <header>
+          <div>
+              <h2>Product List</h2>
+          </div>
+         <div class="btns-header">
+              <button class="btn"><router-link to="addproduct">ADD</router-link></button>
+              <button class="btn" id="delete-product-btn" value="MASS DELETE" @click="massDelete()">MASS DELETE</button>
+         </div>
+      </header>      
+      <hr>
+      <div class="con">
+          <div class="card"  v-bind:key="objData.id" v-for="objData in products" >
+              <input type="checkbox" class="delete-checkbox" :id="objData.id">
+              <ul>
+                  <li>{{objData.sku}}</li>
+                  <li>{{objData.name}}</li>
+                  <li>{{objData.price}}$</li>
+                  <li>{{objData.front_end}}</li>
+
+              </ul>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import AddProduct from './AddProduct';
+export default {
+  name: 'ProductList',
+  components: {
+  AddProduct,
+},
+data() {
+  return {
+      products: [],
+      toDelete:[],
+      host: "https://scandi-task-api.000webhostapp.com/"
+      }
+},  
+methods: {
+    massDelete(){
+      let checks = document.getElementsByClassName("delete-checkbox");
+      for(let i=0;i<checks.length;i++){
+      if(checks[i].checked == true){
+          this.toDelete.push(checks[i].id);
+          }
+      }
+      var data = new FormData();
+      data.append("toDeleteArray", this.toDelete);
+      axios
+        .post(
+          this.host+"back_end/public/home/delete",data
+        ).then(res => {
+            if (res.data.error) {
+              console.log("Error", res);
+
+            } else {
+                this.getAllData();
+            }
+          })
+          .catch(err => {
+            console.log("Error", err);
+          });
+      },
+   getAllData(){
+         axios
+          .get(this.host+'back_end/public/home/getData')
+            .then(res => (this.products = res.data))
+            .catch(err => {
+              console.log("Error", err);
+            });
+            console
+      }
+  
+
+ 
+},
+mounted () {
+this.getAllData();
+},
+}
+</script>
+
+
+
+<style  scoped>
+.container {
+  /* position: relative; */
+  height:100%;
+  width: 100%;
+  overflow: hidden;
+}
+header{
+  margin-top: 50px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.btns-header {
+  margin-right: 50px;
+  width: 20%;
+  display: flex;
+  justify-content: space-around;
+}
+.btn {
+  color: #fff;
+  border: none;
+  background-color: #2298e6;
+  padding: 5px;
+}
+.btn a {
+  text-decoration: none;
+  color: #fff;
+}
+header h2 {
+  margin-left: 20px; 
+}
+
+ul {
+  list-style: none;
+}
+.con {
+  margin: 10px;
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+}
+.card {
+  position: relative;
+  width: 200px;
+  padding: 5px;
+  margin: 10px;
+  border: 1px solid #2298e6;
+  background-color: #eee  ;
+}
+
+.card ul {
+  margin-left: 50%;
+  margin-bottom: 10px;
+  transform: translateX(-70%);
+  text-align: center;
+  color: #000 ;
+}
+
+
+
+</style>
